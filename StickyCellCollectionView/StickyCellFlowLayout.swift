c04eb268d3e9557354e1ad077482b01d4ca944e9
Rattee Wariyawutthiwat <pbserious@gmail.com>
2017-04-04 23:51:07 +0700

    first commit

---
+++ StickyCellCollectionView/StickyCellFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  StickyCellFlowLayout.swift
+//  StickyCellCollectionView
+//
+//  Created by Rattee Wariyawutthiwat on 4/4/2560 BE.
+//  Copyright © 2560 Rattee. All rights reserved.
+//
+
+import Foundation
