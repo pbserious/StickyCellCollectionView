ac8b98c50ecf223417b0bc1f4a86e9351dac8797
Rattee Wariyawutthiwat <pbserious@gmail.com>
2017-04-10 01:49:12 +0700

    init

--- StickyCellCollectionView/StickyCellFlowLayout.swift
+++ StickyCellCollectionView/StickyCellFlowLayout.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2560 Rattee. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StickyCellFlowLayout:UICollectionViewFlowLayout {
+    
+    var stickyIndexPath = [IndexPath(row: 3, section: 0)]
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let layoutAttributes = super.layoutAttributesForElements(in: rect) else { return nil }
+        
+        // Helpers
+        var newLayoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        for layoutAttributesSet in layoutAttributes {
+            if let _ = stickyIndexPath.filter({ indexPath -> Bool in
+                return indexPath == layoutAttributesSet.indexPath
+            }).first {
+                continue
+            }
+            
+            if layoutAttributesSet.representedElementCategory == .cell {
+                // Add Layout Attributes
+                newLayoutAttributes.append(layoutAttributesSet)
+            }
+        }
+        for indexPath in stickyIndexPath {
+            if let attr = layoutAttributesForItem(at: indexPath) {
+                newLayoutAttributes.append(attr)
+            }
+        }
+        return newLayoutAttributes
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let collectionView = collectionView else { return nil }
+        guard let attr = super.layoutAttributesForItem(at: indexPath) else { return nil }
+        if let _ = stickyIndexPath.filter({ sIndexPath -> Bool in
+            return indexPath == sIndexPath
+        }).first {
+            attr.zIndex += 10
+            if attr.frame.origin.y < collectionView.contentOffset.y {
+                attr.frame.origin.y = collectionView.contentOffset.y
+            }
+        }
+        return attr
+    }
+}
--- StickyCellCollectionView/ViewController.swift
+++ StickyCellCollectionView/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let count = 30
+    @IBOutlet weak var collectionView:UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let layout = collectionView.collectionViewLayout as? StickyCellFlowLayout {
+            layout.stickyIndexPath = []
+            for index in 0..<count {
+                if index % 4 == 0 {
+                    layout.stickyIndexPath.append(IndexPath(row: index, section: 0))
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getRandomColor() -> UIColor{
+        
+        let randomRed:CGFloat = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        let randomGreen:CGFloat = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        let randomBlue:CGFloat = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
+    }
 
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        if indexPath.row % 4 == 0 {
+            cell.backgroundColor = getRandomColor()
+        } else {
+            cell.backgroundColor = UIColor.red
+        }
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    /*override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 40)
+    }*/
 }
 
